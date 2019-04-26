e2509bf064d2ae63aedabd4fc4ad90cfc7c16c4b
anddev404 <36824866+anddev404@users.noreply.github.com>
2019-04-26 20:56:52 +0200

    Dodano przykładowe dane do klasy People

--- app/src/main/java/com/anddev/movieguide/MainActivity.java
+++ app/src/main/java/com/anddev/movieguide/MainActivity.java
@@ -3,11 +3,16 @@ package com.anddev.movieguide;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.anddev.movieguide.model.People;
+import com.google.gson.Gson;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        People p = People.getExamplePeople();
     }
+
 }
--- app/src/main/java/com/anddev/movieguide/model/People.java
+++ app/src/main/java/com/anddev/movieguide/model/People.java
@@ -1,8 +1,10 @@
 package com.anddev.movieguide.model;
 
+import com.google.gson.Gson;
+
 import java.util.List;
 
-public abstract class People {
+public class People {
 
     private String imdb_id;
     private boolean adult;
@@ -112,5 +114,38 @@ public abstract class People {
     public void setBirthday(String birthday) {
         this.birthday = birthday;
     }
-    
+
+    public static People getExamplePeople() {
+        try {
+            String jsonString = "{\n" +
+                    "  \"birthday\": \"1967-07-26\",\n" +
+                    "  \"known_for_department\": \"Acting\",\n" +
+                    "  \"id\": 976,\n" +
+                    "  \"name\": \"Jason Statham\",\n" +
+                    "  \"also_known_as\": [\n" +
+                    "    \"Джейсон Стейтем\",\n" +
+                    "    \"Джейсон Стэйтем\",\n" +
+                    "    \"جيسون ستاثام\",\n" +
+                    "    \"제이슨 스테이섬\",\n" +
+                    "    \"ジェイソン・ステイサム\",\n" +
+                    "    \"เจสัน สเตธัม\",\n" +
+                    "    \"傑森·史塔森\",\n" +
+                    "    \"Джейсън Стейтъм\",\n" +
+                    "    \"ჯეისონ სტეტჰემი\"\n" +
+                    "  ],\n" +
+                    "  \"gender\": 2,\n" +
+                    "  \"biography\": \"\",\n" +
+                    "  \"popularity\": 15.98,\n" +
+                    "  \"place_of_birth\": \"Shirebrook, Derbyshire, England, UK\",\n" +
+                    "  \"profile_path\": \"/PhWiWgasncGWD9LdbsGcmxkV4r.jpg\",\n" +
+                    "  \"adult\": false,\n" +
+                    "  \"imdb_id\": \"nm0005458\"\n" +
+                    "}";
+            Gson gson = new Gson();
+            return gson.fromJson(jsonString, People.class);
+        } catch (Exception e) {
+            return null;
+        }
+
+    }
 }
