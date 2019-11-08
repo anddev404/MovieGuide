d8fed42101c3fa3fb092ca0fd8a5f5284bdf2f7d
anddev404 <36824866+anddev404@users.noreply.github.com>
2019-11-08 20:43:51 +0100

    Dodano obsługę wątków w aktywności MovieActivity

--- app/src/main/java/com/anddev/movieguide/movieBranch/MovieActvity.java
+++ app/src/main/java/com/anddev/movieguide/movieBranch/MovieActvity.java
@@ -2,14 +2,26 @@ package com.anddev.movieguide.movieBranch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.anddev.movieguide.R;
+import com.anddev.movieguide.tools.ConnectionInterface;
 
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Background;
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_movie)
 public class MovieActvity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_movie_actvity);
+    @AfterViews
+    public void onCreate() {
+
     }
+
+    @Background
+    void downloadMovieInBackground() {
+
+    }
+
 }
