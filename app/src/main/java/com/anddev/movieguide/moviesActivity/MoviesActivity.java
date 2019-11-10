5775e045c6d5fe745049ade965f2f3a16e6367f8
anddev404 <36824866+anddev404@users.noreply.github.com>
2019-11-10 20:37:56 +0100

    Utworzono aktywność MoviesActivity
    Wyświetla listę filmów,

---
+++ app/src/main/java/com/anddev/movieguide/moviesActivity/MoviesActivity.java
@@ -0,0 +1,15 @@
+package com.anddev.movieguide.moviesActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.anddev.movieguide.R;
+
+public class MoviesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movies);
+    }
+}
