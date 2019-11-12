142f7d789478b4f3ffa8d39555c952bcf0893022
anddev404 <36824866+anddev404@users.noreply.github.com>
2019-11-12 19:45:13 +0100

    Update MoviesActivity
    Dodano pobieranie filmów.

--- app/src/main/java/com/anddev/movieguide/moviesActivity/MoviesActivity.java
+++ app/src/main/java/com/anddev/movieguide/moviesActivity/MoviesActivity.java
@@ -1,24 +1,88 @@
 package com.anddev.movieguide.moviesActivity;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.widget.Toast;
 
 import com.anddev.movieguide.R;
-import com.anddev.movieguide.movieActivity.MovieActvity_;
+import com.anddev.movieguide.model.Movies;
+import com.anddev.movieguide.tools.ConnectionInterface;
+import com.anddev.movieguide.tools.RetrofitTools;
 
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Background;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.UiThread;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+@EActivity(R.layout.activity_movies)
 public class MoviesActivity extends AppCompatActivity {
 
     Activity activity;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_movies);
+    ConnectionInterface client;
+    Movies movies;
+
+    @AfterViews
+    public void onCreate() {
         activity = this;
 
-//        Intent intent = new Intent(activity, MovieActvity_.class);
-//        activity.startActivity(intent);
+        try {
+
+            client = RetrofitTools.getConnectionInterface();
+            downloadMoviesInBackground(client, RetrofitTools.API_KEY, RetrofitTools.LANGUAGE, 1);
+
+        } catch (Exception e) {
+
+        }
+
+    }
+
+    @Background
+    void downloadMoviesInBackground(ConnectionInterface client, String apiKey, String language, Integer page) {
+        try {
+
+
+            Call<Movies> call = client.popularMovie(apiKey, language, page);
+
+            call.enqueue(new Callback<Movies>() {
+
+                @Override
+                public void onResponse(Call<Movies> call, Response<Movies> response) {
+
+                    if (response.code() == 200) {
+
+                        movies = response.body();
+
+                    } else {
+
+                        showError("Nie można pobrać odpowiednich danych");
+
+                    }
+
+                }
+
+                @Override
+                public void onFailure(Call<Movies> call, Throwable t) {
+
+                    showError("Brak połączenia internetowego!");
+
+                }
+            });
+        } catch (Throwable e) {
+            showError("Nieoczekiwany błąd!");
+
+        }
+
+    }
+
+    @UiThread
+    public void showError(String message) {
+
+        Toast.makeText(activity, message, Toast.LENGTH_SHORT).show();
+
     }
 }
