43084c98f8f8cdb326c2c928029d4fc8df094e9f
anddev404 <36824866+anddev404@users.noreply.github.com>
2019-11-21 21:29:12 +0100

    Update SearchEngineActivity
    Dodano navigationDrawer.

--- app/src/main/java/com/anddev/movieguide/searchEngineActivity/SearchEngineActivity.java
+++ app/src/main/java/com/anddev/movieguide/searchEngineActivity/SearchEngineActivity.java
@@ -1,9 +1,11 @@
 package com.anddev.movieguide.searchEngineActivity;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.anddev.movieguide.R;
+import com.anddev.movieguide.tools.NavigationDrawerTools;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
@@ -11,9 +13,13 @@ import org.androidannotations.annotations.EActivity;
 @EActivity(R.layout.activity_search_engine)
 public class SearchEngineActivity extends AppCompatActivity {
 
+    Activity activity;
+    NavigationDrawerTools navigationDrawer;
 
     @AfterViews
     public void onCreate() {
+        activity = this;
+        navigationDrawer = new NavigationDrawerTools(activity);
 
     }
 }
