d764471724205691b055e7625bd10ac4d936d91c
anddev404 <36824866+anddev404@users.noreply.github.com>
2020-02-19 18:26:04 +0100

    Update MoviesFragment.java

--- app/src/main/java/com/anddev/movieguide/moviesActivity/MoviesFragment.java
+++ app/src/main/java/com/anddev/movieguide/moviesActivity/MoviesFragment.java
@@ -1,11 +1,12 @@
 package com.anddev.movieguide.moviesActivity;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,8 +14,7 @@ import android.view.ViewGroup;
 import com.anddev.movieguide.R;
 import com.anddev.movieguide.model.Genre;
 import com.anddev.movieguide.model.Movies;
-import com.anddev.movieguide.movieActivity.MovieActvity_;
-import com.anddev.movieguide.tools.RecyclerItemClickListener;
+import com.anddev.movieguide.tools.ActionBarTools;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -26,9 +26,11 @@ public class MoviesFragment extends Fragment {
     Movies movies;
     Genre genres;
     MoviesAdapter adapter;
+    MoviesGridViewAdapter adapterGridView;
 
     @BindView(R.id.movies_list_recycler_view)
     public RecyclerView moviesListRecyclerView;
+    int viewType = 0;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -37,46 +39,78 @@ public class MoviesFragment extends Fragment {
         rootView = inflater.inflate(R.layout.fragment_movies, container, false);
         activity = getActivity();
         ButterKnife.bind(this, rootView);
-        moviesListRecyclerView.setLayoutManager(new LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false));
 
         if (movies != null) {
-            setData(movies, genres);
+            initializeRecyclerViewAndSetAdapter();
         }
         return rootView;
+    }
+
+    public void setViewType(int viewType) {
+        this.viewType = viewType;
+    }
+
+    public void initializeRecyclerViewAndSetAdapter() {
+        Log.d("MOVIES_FRAGMENT", "initialize view");
+
+        try {
+            if (viewType == ActionBarTools.GRID1_VIEW) {
+                moviesListRecyclerView.setLayoutManager(new GridLayoutManager(activity, 1));
+                adapterGridView = new MoviesGridViewAdapter(activity, movies);
+                moviesListRecyclerView.setAdapter(adapterGridView);
+
+            } else if (viewType == ActionBarTools.GRID2_VIEW) {
+                moviesListRecyclerView.setLayoutManager(new GridLayoutManager(activity, 2));
+                adapterGridView = new MoviesGridViewAdapter(activity, movies);
+                moviesListRecyclerView.setAdapter(adapterGridView);
+
+
+            } else if (viewType == ActionBarTools.GRID3_VIEW) {
+                moviesListRecyclerView.setLayoutManager(new GridLayoutManager(activity, 3));
+                adapterGridView = new MoviesGridViewAdapter(activity, movies);
+                moviesListRecyclerView.setAdapter(adapterGridView);
+
+
+            } else if (viewType == ActionBarTools.GRID4_VIEW) {
+                moviesListRecyclerView.setLayoutManager(new GridLayoutManager(activity, 4));
+                adapterGridView = new MoviesGridViewAdapter(activity, movies);
+                moviesListRecyclerView.setAdapter(adapterGridView);
+
+
+            } else {
+                moviesListRecyclerView.setLayoutManager(new LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false));
+                if (genres == null) {
+                    adapter = new MoviesAdapter(activity, movies);
+
+                } else {
+                    adapter = new MoviesAdapter(activity, movies, genres);
+
+                }
+                moviesListRecyclerView.setAdapter(adapter);
+
+            }
+
+        } catch (Exception e) {
+
+        }
+
+
     }
 
     public void setData(final Movies movies) {
 
         this.movies = movies;
 
-        adapter = new MoviesAdapter(activity, movies);
-        moviesListRecyclerView.setAdapter(adapter);
-//
-//        moviesListRecyclerView.addOnItemTouchListener(
-//                new RecyclerItemClickListener(getActivity(), moviesListRecyclerView, new RecyclerItemClickListener.OnItemClickListener() {
-//                    @Override
-//                    public void onItemClick(View view, int position) {
-//                        Intent intent = new Intent(getActivity(), MovieActvity_.class);
-//                        intent.putExtra("Id", movies.getResults().get(position).getId());
-//                        startActivity(intent);
-//                    }
-//
-//                    @Override
-//                    public void onLongItemClick(View view, int position) {
-//                    }
-//
-//                })
-//        );
-//
-//
+        initializeRecyclerViewAndSetAdapter();
+
     }
 
     public void setData(final Movies movies, Genre genres) {
 
         this.movies = movies;
+        this.genres = genres;
 
-        adapter = new MoviesAdapter(activity, movies, genres);
-        moviesListRecyclerView.setAdapter(adapter);
+        initializeRecyclerViewAndSetAdapter();
     }
 
     public void setGenres(Genre genres) {
@@ -89,7 +123,12 @@ public class MoviesFragment extends Fragment {
 
         try {
             this.movies.getResults().addAll(movies.getResults());
-            adapter.notifyDataSetChanged();
+            if (adapter != null) {
+                adapter.notifyDataSetChanged();
+            }
+            if (adapterGridView != null) {
+                adapterGridView.notifyDataSetChanged();
+            }
         } catch (Exception e) {
         }
     }
