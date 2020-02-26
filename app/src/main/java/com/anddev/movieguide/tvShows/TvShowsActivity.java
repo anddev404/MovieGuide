fea29a857fb9c8696eff7e09fd117453efca0b64
anddev404 <36824866+anddev404@users.noreply.github.com>
2020-02-26 22:42:36 +0100

    Create TvShowsActivity

---
+++ app/src/main/java/com/anddev/movieguide/tvShows/TvShowsActivity.java
@@ -0,0 +1,18 @@
+package com.anddev.movieguide.tvShows;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.anddev.movieguide.R;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_tv_shows)
+public class TvShowsActivity extends AppCompatActivity {
+
+    @AfterViews
+    public void onCreate() {
+
+    }
+}
