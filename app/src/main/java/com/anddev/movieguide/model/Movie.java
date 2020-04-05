93093ff03b61b4d5b1747c88a3dd9174732f8bb5
anddev404 <>
2020-04-05 22:20:50 +0200

    Refaktoryzacja Movie.java

--- app/src/main/java/com/anddev/movieguide/model/Movie.java
+++ app/src/main/java/com/anddev/movieguide/model/Movie.java
@@ -4,31 +4,17 @@ import java.util.List;
 
 public class Movie {
 
-    private int vote_count;
     private double vote_average;
-    private boolean video;
     private String title;
-    private String tagline;
-    private String status;
-    private List<Spoken_languages> spoken_languages;
     private int runtime;
-    private int revenue;
     private String release_date;
     private List<Production_countries> production_countries;
-    private List<Production_companies> production_companies;
     private String poster_path;
-    private double popularity;
     private String overview;
     private String original_title;
-    private String original_language;
-    private String imdb_id;
     private int id;
-    private String homepage;
     private List<Genres> genres;
-    private int budget;
-    private Belongs_to_collection belongs_to_collection;
     private String backdrop_path;
-    private boolean adult;
 
     public String genresToString() {
         try {
@@ -70,13 +56,6 @@ public class Movie {
 
     }
 
-    public int getVote_count() {
-        return vote_count;
-    }
-
-    public void setVote_count(int vote_count) {
-        this.vote_count = vote_count;
-    }
 
     public double getVote_average() {
         return vote_average;
@@ -86,13 +65,6 @@ public class Movie {
         this.vote_average = vote_average;
     }
 
-    public boolean getVideo() {
-        return video;
-    }
-
-    public void setVideo(boolean video) {
-        this.video = video;
-    }
 
     public String getTitle() {
         return title;
@@ -102,44 +74,12 @@ public class Movie {
         this.title = title;
     }
 
-    public String getTagline() {
-        return tagline;
-    }
-
-    public void setTagline(String tagline) {
-        this.tagline = tagline;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public List<Spoken_languages> getSpoken_languages() {
-        return spoken_languages;
-    }
-
-    public void setSpoken_languages(List<Spoken_languages> spoken_languages) {
-        this.spoken_languages = spoken_languages;
-    }
-
     public int getRuntime() {
         return runtime;
     }
 
     public void setRuntime(int runtime) {
         this.runtime = runtime;
-    }
-
-    public int getRevenue() {
-        return revenue;
-    }
-
-    public void setRevenue(int revenue) {
-        this.revenue = revenue;
     }
 
     public String getRelease_date() {
@@ -158,28 +98,12 @@ public class Movie {
         this.production_countries = production_countries;
     }
 
-    public List<Production_companies> getProduction_companies() {
-        return production_companies;
-    }
-
-    public void setProduction_companies(List<Production_companies> production_companies) {
-        this.production_companies = production_companies;
-    }
-
     public String getPoster_path() {
         return poster_path;
     }
 
     public void setPoster_path(String poster_path) {
         this.poster_path = poster_path;
-    }
-
-    public double getPopularity() {
-        return popularity;
-    }
-
-    public void setPopularity(double popularity) {
-        this.popularity = popularity;
     }
 
     public String getOverview() {
@@ -198,36 +122,12 @@ public class Movie {
         this.original_title = original_title;
     }
 
-    public String getOriginal_language() {
-        return original_language;
-    }
-
-    public void setOriginal_language(String original_language) {
-        this.original_language = original_language;
-    }
-
-    public String getImdb_id() {
-        return imdb_id;
-    }
-
-    public void setImdb_id(String imdb_id) {
-        this.imdb_id = imdb_id;
-    }
-
     public int getId() {
         return id;
     }
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    public String getHomepage() {
-        return homepage;
-    }
-
-    public void setHomepage(String homepage) {
-        this.homepage = homepage;
     }
 
     public List<Genres> getGenres() {
@@ -238,57 +138,12 @@ public class Movie {
         this.genres = genres;
     }
 
-    public int getBudget() {
-        return budget;
-    }
-
-    public void setBudget(int budget) {
-        this.budget = budget;
-    }
-
-    public Belongs_to_collection getBelongs_to_collection() {
-        return belongs_to_collection;
-    }
-
-    public void setBelongs_to_collection(Belongs_to_collection belongs_to_collection) {
-        this.belongs_to_collection = belongs_to_collection;
-    }
-
     public String getBackdrop_path() {
         return backdrop_path;
     }
 
     public void setBackdrop_path(String backdrop_path) {
         this.backdrop_path = backdrop_path;
-    }
-
-    public boolean getAdult() {
-        return adult;
-    }
-
-    public void setAdult(boolean adult) {
-        this.adult = adult;
-    }
-
-    public static class Spoken_languages {
-        private String name;
-        private String iso_639_1;
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getIso_639_1() {
-            return iso_639_1;
-        }
-
-        public void setIso_639_1(String iso_639_1) {
-            this.iso_639_1 = iso_639_1;
-        }
     }
 
     public static class Production_countries {
@@ -312,87 +167,9 @@ public class Movie {
         }
     }
 
-    public static class Production_companies {
-        private String origin_country;
-        private String name;
-        private String logo_path;
-        private int id;
-
-        public String getOrigin_country() {
-            return origin_country;
-        }
-
-        public void setOrigin_country(String origin_country) {
-            this.origin_country = origin_country;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getLogo_path() {
-            return logo_path;
-        }
-
-        public void setLogo_path(String logo_path) {
-            this.logo_path = logo_path;
-        }
-
-        public int getId() {
-            return id;
-        }
-
-        public void setId(int id) {
-            this.id = id;
-        }
-    }
-
     public static class Genres {
         private String name;
         private int id;
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public int getId() {
-            return id;
-        }
-
-        public void setId(int id) {
-            this.id = id;
-        }
-    }
-
-    public static class Belongs_to_collection {
-        private String backdrop_path;
-        private String poster_path;
-        private String name;
-        private int id;
-
-        public String getBackdrop_path() {
-            return backdrop_path;
-        }
-
-        public void setBackdrop_path(String backdrop_path) {
-            this.backdrop_path = backdrop_path;
-        }
-
-        public String getPoster_path() {
-            return poster_path;
-        }
-
-        public void setPoster_path(String poster_path) {
-            this.poster_path = poster_path;
-        }
 
         public String getName() {
             return name;
