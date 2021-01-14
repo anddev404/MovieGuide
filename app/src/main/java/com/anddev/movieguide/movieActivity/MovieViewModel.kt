4ae4d6f22746e9bc9cbda0ca7f30bde00d89b041
anddev404 <anddev404@gmail.com>
2021-01-14 21:40:15 +0100

    Update MovieViewModel.kt

--- app/src/main/java/com/anddev/movieguide/movieActivity/MovieViewModel.kt
+++ app/src/main/java/com/anddev/movieguide/movieActivity/MovieViewModel.kt
@@ -1,4 +1,16 @@
 package com.anddev.movieguide.movieActivity
 
 class MovieViewModel {
+    var title: String = ""
+    var originalTitle: String = ""
+    var overview: String = ""
+    var releaseDate: String = ""
+    var genres: String = ""
+    var voteAverage: String = ""
+    var runtime: String = ""
+    var productionCountries: String = ""
+
+//    voteAverage.setText(getPercentageFromDouble(movie.getVote_average(), 10));
+//    runtime.setText(movie.getRuntime() + " min.");
+//    productionCountries.setText(movie.productionCountriesToString());
 }
